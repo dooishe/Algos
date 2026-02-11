11e3291f66124fdb7f599421ba9a3563abce2e92
dooishe <tugievzx@mail.ru>
2026-02-11 18:03:36 +0300

    добавил вычисления количества простых чисел в диапазоне

--- IsSimpleNumber/Main.java
+++ IsSimpleNumber/Main.java
@@ -3,13 +3,26 @@ package IsSimpleNumber;
 public class Main {
   public static void main(String[] args) {
     int s = 4;
-    boolean prime = true;
+    boolean prime1 = true;
     for (int i = 2; i <= Math.sqrt(s); i++) {
       if(s % i == 0) {
-        prime = false;
+        prime1 = false;
         break;
       }
     }
-    System.out.println(prime);
+    System.out.println(prime1);
+    /////////////////////////////////////
+    int counter = 0;
+    for (int m = 2; m <= 7; m++) {
+      boolean prime2 = true;
+      for (int i = 2; i <= Math.sqrt(m); i++) {
+      if(m % i == 0) {
+        prime2 = false;
+        break;
+      }
+    }
+    if(prime2) counter++;
+    }
+    System.out.println(counter);
   }
 }
